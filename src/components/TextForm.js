7c777d711221d12657620c86329892a4c66f3c3d
Rakesh-AC <rakesh.ac@parachutetech.ai>
2023-11-18 22:54:41 +0530

    Enhancing the text utils by adding more logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,14 +15,41 @@ export default function TextForm(props) {
     setText("");
   }
 
+  const toLoCase = () =>{
+    setText(text.toLowerCase());
+  }
+
+  
+
   return (
+    <>
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className='form-control' value={text} onChange={handleChange} name="" id="myBox" cols="30" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={convertToUppercase}>Convert to Uppercase</button>
+        <button className="btn btn-primary ms-2" onClick={toLoCase}>Convert to Uppercase</button>
+
         <button className="btn btn-secondary ms-2" onClick={clearText}>clear</button>
     </div>
+
+
+    <div className='mt-3'>
+      <h1>Your Text Summary</h1>
+      <p>{(text.length==0)?0:text.trim().split(" ").length} Words and {text.length} Characters</p> 
+    </div>
+
+  
+
+
+    <div className='mt-3'>
+      <h1>Preview</h1>
+      <p>{text}</p>
+    </div>
+
+    </>
+
+
   )
 }

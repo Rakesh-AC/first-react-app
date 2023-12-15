ce773a2ecf1c5cbfd2231bd5480d059237105661
Rakesh-AC <rakesh.ac@parachutetech.ai>
2023-12-15 21:55:07 +0530

    commiting my code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,8 +18,15 @@ export default function Navbar(props) {
               <li className="nav-item">
                 <a className="nav-link" href="#">{props.aboutText}</a>
               </li>
-              
             </ul>
+
+            <div className="ms-auto"> {/* Add ms-auto class to push to the end */}
+              <div className={`form-check form-switch text-${props.mode==='light'?"dark":"light"}`} >
+                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckChecked"  />
+                <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Dark Mode</label>
+              </div>
+            </div>
+
           </div>
         </div>
       </nav>
